7974df23139d7d1cd20d8ab86f0aa7b27f532865
RIZAL IRIANSYAH <43509313+rizaliriansyah@users.noreply.github.com>
2018-10-02 17:00:14 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form action="prosesregis.php" method="POST">
+	<center>
+		<h1>Pendaftaran Mahasiswa</h1>
+		<table>
+			<tr>
+				<td>NIM</td>
+				<td>:</td>
+				<td><input type="text" name="nim"></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td>:</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><input type="submit" name="submit" value="daftar"></td>
+			</tr>
+		</table>
+	</center>
+</form>
